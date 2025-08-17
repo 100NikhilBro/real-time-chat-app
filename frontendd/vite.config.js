90b216c14b8819e6c40f993e8928d8872a944970
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-08-17 17:14:56 +0530

    Update vite.config.js

--- frontendd/vite.config.js
+++ frontendd/vite.config.js
@@ -4,13 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [react(), tailwindcss(), ],
-    server: {
-        proxy: {
-            '/api/v1': {
-                target: "http://localhost:6174",
-                changeOrigin: true,
-                secure: false,
-            }
-        }
-    }
+    // server: {
+    //     proxy: {
+    //         '/api/v1': {
+    //             target: "http://localhost:6174",
+    //             changeOrigin: true,
+    //             secure: false,
+    //         }
+    //     }
+    // }
 })
