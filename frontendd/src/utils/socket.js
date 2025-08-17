5de0df37311c1b68f76318872c40bf78388010cd
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-08-17 17:52:56 +0530

    Update socket.js

--- frontendd/src/utils/socket.js
+++ frontendd/src/utils/socket.js
@@ -2,11 +2,12 @@ import { io } from "socket.io-client";
 
 const token = localStorage.getItem("zapchat_token");
 
-const socket = io("http://localhost:6174", {
-    auth: {
-        token: token,
-    },
-    withCredentials: true,
+
+const socket = io("https://real-time-chat-app-wpf4.onrender.com", {
+  auth: {
+    token: token,
+  },
+  withCredentials: true,
 });
 
 export default socket;
